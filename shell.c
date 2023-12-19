f0a595e618c791835bd1d81accb5f5e89f2b190b
Alfredolsl <alfredo.445566@gmail.com>
2023-12-19 10:20:27 -0800

    added infinite loop with free memory function when exit called

--- shell.c
+++ shell.c
@@ -5,7 +5,7 @@
  * @ac: arg counter
  * @argv: arg vector
  *
- * Return: 0 if successful, otherwise 1
+ * Return: 0 if successful, otherwise -1
  */
 
 int main(int ac, char **argv)
@@ -13,14 +13,24 @@ int main(int ac, char **argv)
 	char *prompt = "$ ";
 	char *lineptr;
 	size_t n = 0;
+	ssize_t nchars_read;
 
-	//while (1)
-	//{
-	printf("%s", prompt);
-	getline(&lineptr, &n, stdin);
-	printf("%s\n", lineptr);
+	while (1)
+	{
+		printf("%s", prompt);
+		nchars_read = getline(&lineptr, &n, stdin);
+
+		if (nchars_read == -1)
+		{
+			printf("Exiting shell...\n");
+			free(lineptr);
+			return (-1);
+		}
+
+		printf("%s\n", lineptr);
+	}
+
 	free(lineptr);
-	//}
 
 	return (0);
 }
